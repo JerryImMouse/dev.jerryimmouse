edd84277e550709f8a323b2734c5169ef21638d8
JerryImMouse <toperkas@gmail.com>
2025-06-05 06:21:19 +0500

    fix(bg): fix gray bg

--- src/components/page.rs
+++ src/components/page.rs
@@ -7,7 +7,7 @@ use crate::components::{DefaultToaster, Footer, NavBar, ParticlesBackground};
 pub fn Page() -> impl IntoView {
     view! {
         <ParticlesBackground />
-        <div class="relative z-0 flex flex-col min-h-screen text-gray-100 bg-transparent scroll-smooth">
+        <div class="flex flex-col min-h-screen text-gray-100 bg-transparent scroll-smooth">
             <NavBar />
             <Outlet />
             <DefaultToaster />
